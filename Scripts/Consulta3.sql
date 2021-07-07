59dc97fc4e22a092fe8e53bd52c339b41313b806
jeifkol123 <77014694+jeifkol123@users.noreply.github.com>
2021-07-07 10:14:52 -0500

    Base Datos

--- Scripts/Consulta3.sql
+++ Scripts/Consulta3.sql
@@ -1,14 +1,29 @@
-select tabla_nino.nombrenino, tabla_nino.apellidonino,
-case
-  when tabla_nino.idtutor is not null then 'no tiene padres ' 
-  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=1  then 'no tiene de forma activa a su padre' 
-  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=2  then 'no tiene de forma activa a su madre' end 
-as Situacion, 
-case
-  when tabla_nino.idtutor is not null then 'tutor' 
-  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=1  then 'madre' 
-  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=2  then 'padre' end 
- as Representante
-from tabla_nino 
-left join tabla_padre on tabla_nino.idnino=tabla_padre.idpadre 
-left join tabla_genero on tabla_padre.idgenero=tabla_genero.idgenero;
+
+select tabla_programa.nombreprograma, 
+count(*) as Total,
+CASE
+	WHEN tabla_genero.idgenero = 1 THEN 'Hombre'
+WHEN tabla_genero.idgenero = 2 THEN 'Mujer'
+END AS Tipo
+from tabla_propuesta
+inner join tabla_profesional
+on tabla_propuesta.idprofesional = tabla_profesional.idprofesional
+inner join tabla_genero
+on tabla_profesional.idgenero = tabla_genero.idgenero
+inner join tabla_programa
+on tabla_programa.idprograma = tabla_propuesta.idprograma
+
+group by tabla_genero.genero,
+tabla_programa.nombreprograma,
+tabla_profesional.idgenero,tabla_genero.idgenero
+order by tabla_genero.genero ;
+/*para comprobacion de que es valido el Script de consulta cambiamos de genero a uno de los profesionales para 
+que se refleje que si hace la debida consulta detallando cuantos profesionales son hombres y mujeres en cada 
+programa
+
+update public.tabla_profesional
+	set  idgenero=2
+	where tabla_profesional.idprofesional=5;
+		select * from tabla_profesional;
+		
+		*/
