a7960da5e05abaf4d25a38812615d873408ede1c
jeifkol123 <77014694+jeifkol123@users.noreply.github.com>
2021-07-07 09:42:18 -0500

    Base Datos

--- Scripts/Consulta 1.sql
+++ Scripts/Consulta 1.sql
@@ -1,14 +1,14 @@
 select tabla_nino.nombrenino, tabla_nino.apellidonino,
 case
-  when tabla_nino.idtutor is not null then 'no tiene padres ' 
-  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=1  then 'no tiene de forma activa a su padre' 
-  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=2  then 'no tiene de forma activa a su madre' end 
-as Situacion, 
+  when tabla_nino.idtutor is not null then 'no tiene de forma activa a sus padres' 
+  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=1  then 'no tiene de forma activa a su madre' 
+  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=2  then 'no tiene de forma activa a su padre' end 
+as Situación, 
 case
   when tabla_nino.idtutor is not null then 'tutor' 
-  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=1  then 'madre' 
-  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=2  then 'padre' end 
- as Representante
+  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=1  then 'padre' 
+  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=2  then 'madre' end 
+ as Representante 
 from tabla_nino 
 left join tabla_padre on tabla_nino.idnino=tabla_padre.idpadre 
 left join tabla_genero on tabla_padre.idgenero=tabla_genero.idgenero;
