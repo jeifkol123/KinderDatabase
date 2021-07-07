f62e6f42fef0bcbcfb8d5ed33ebfef27503796cc
jeifkol123 <77014694+jeifkol123@users.noreply.github.com>
2021-07-07 10:02:35 -0500

    Base Datos

--- Scripts/SCRIPT GENERAL KINDERDATABASE.sql
+++ Scripts/SCRIPT GENERAL KINDERDATABASE.sql
@@ -487,21 +487,21 @@ INSERT INTO tabla_rendimiento_nino(idrendimientonino, idnino, idprograma, idesca
 VALUES (18, 6, 3, 2);
 
 /*CONSULTA 1*/
-
 select tabla_nino.nombrenino, tabla_nino.apellidonino,
 case
-  when tabla_nino.idtutor is not null then 'no tiene padres ' 
-  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=1  then 'no tiene de forma activa a su padre' 
-  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=2  then 'no tiene de forma activa a su madre' end 
-as Situacion, 
+  when tabla_nino.idtutor is not null then 'no tiene de forma activa a sus padres' 
+  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=1  then 'no tiene de forma activa a su madre' 
+  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=2  then 'no tiene de forma activa a su padre' end 
+as Situación, 
 case
   when tabla_nino.idtutor is not null then 'tutor' 
-  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=1  then 'madre' 
-  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=2  then 'padre' end 
- as Representante
+  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=1  then 'padre' 
+  when tabla_nino.idpadre  is not null and tabla_padre.idgenero=2  then 'madre' end 
+ as Representante 
 from tabla_nino 
 left join tabla_padre on tabla_nino.idnino=tabla_padre.idpadre 
 left join tabla_genero on tabla_padre.idgenero=tabla_genero.idgenero;
+
 
 /*CONSULTA 2*/
 select  tabla_programa.nombreprograma, count(*) as total, tabla_escala_rendimiento.escalarendimiento
