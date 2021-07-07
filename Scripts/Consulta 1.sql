ce33e7503447b298669c6f8b70c35b6aa312eaef
jeifkol123 <77014694+jeifkol123@users.noreply.github.com>
2021-07-07 17:48:32 -0500

    Base Datos

--- Scripts/Consulta 1.sql
+++ Scripts/Consulta 1.sql
@@ -10,5 +10,5 @@ case
   when tabla_nino.idpadre  is not null and tabla_padre.idgenero=2  then 'madre' end 
  as Representante 
 from tabla_nino 
-left join tabla_padre on tabla_nino.idnino=tabla_padre.idpadre 
+left join tabla_padre on tabla_nino.idpadre=tabla_padre.idpadre 
 left join tabla_genero on tabla_padre.idgenero=tabla_genero.idgenero;
